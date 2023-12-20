2dca4bc4018d1027d5dcf5a7dfdf925f030ad6b1
leosaq <leandrosaquisari7@gmail.com>
2023-12-20 16:38:56 -0500

    Ejercicio

---
+++ Ejemplosc3/src/paquete05/Ejemplo002.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete05;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author LAB.ELECT
+ */
+public class Ejemplo002 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        int[][] arreglo = {{10, 41, 40},
+                            {1, 2, 3}, 
+                            {1, 12, 4}};
+        double suma = 0;
+
+        for (int fila = 0; fila < arreglo.length; fila++) {
+            for (int colum = 0; colum < arreglo.length; colum++) {
+                if (fila == colum){
+                    suma = suma  + arreglo[fila][colum];
+                }
+            }
+        }
+        System.out.printf("La suma de los valores es: %.0f\n", suma);
+    }
+}
+/*
+En este programa primeramente se declaran los valores que van a ir en las 
+respectivas filas y columnas para ende crear dos ciclos for que van a recorrer 
+todo el programa, en el segundo ciclo for utilice "arreglo[fila]" para poder 
+*/
